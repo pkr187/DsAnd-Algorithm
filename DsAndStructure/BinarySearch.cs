ee44f11bf831600c5e0233100fec8890044e74c4
pkr187 <pankajroman187@gmail.com>
2022-09-22 19:25:14 +0530

    [pankaj] Add:InsertionSort

---
+++ DsAndStructure/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsAndStructure
+{
+    internal class BinarySearch
+    {
+        public void Search(string filePath)
+        {
+            string data = File.ReadAllText(filePath);
+            string[] words = data.Split(",");
+            Console.WriteLine(" Enter word to search");
+            string search = Console.ReadLine();
+            foreach (var word in words)
+            {
+                if (search.ToLower().Equals(word.ToLower()))
+                {
+                    Console.WriteLine("word found");
+                    return;
+                }
+            }
+            Console.WriteLine("word not found");
+        }
+    }
+}
---
+++ DsAndStructure/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DsAndStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs \n2.Binary search \n3.Insertion sort\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 2:
+                        BinarySearch binarysearch = new BinarySearch();
+                        binarysearch.Search(@"C:\Users\accer\Desktop\Bridgelabz\DsAndAlgorithm\DsAndAlgorithm\Word.txt");
+                        break;
+                    case 3:
+                        InsertionSort insertionsort = new InsertionSort();
+                        insertionsort.Insertion();
+                        break;
+                }
+            }
+        }
+    }
+}
