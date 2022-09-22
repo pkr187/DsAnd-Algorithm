f35fc8961a1fa55d43ea9bf676b7764df684c272
pkr187 <pankajroman187@gmail.com>
2022-09-22 20:02:28 +0530

    [pankaj] Add:MergeSort

---
+++ DsAndAlgorithm/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace DsAndAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs \n2.Binary search \n3.Insertion sort \n4.Bubble sort  \n5.Merge Sort");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 2:
+                        BinarySearch binarysearch = new BinarySearch();
+                        binarysearch.Search(@"C:\Users\accer\Desktop\Bridgelabz\DsAndAlgorithm\DsAndAlgorithm\Word.txt");
+                        break;
+                    case 3:
+                        InsertionSort insertionsort = new InsertionSort();
+                        insertionsort.Insertion();
+                        break;
+                    case 4:
+                        BubbleSort bubblesort = new BubbleSort();
+                        bubblesort.Sort(@"C:\Users\accer\Desktop\Bridgelabz\DsAndAlgorithm\DsAndAlgorithm\Word.txt");
+                        break;
+                    case 5:
+                        MergeSort mergeSort = new MergeSort();
+                        int n = 8;
+                        int[] array = { 49, 3, 53, 21, 27, 75, 50, 41 };
+                        Console.WriteLine("Merge Sort");
+                        Console.Write("Initial array is: ");
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write(array[i] + " ");
+                        }
+                        mergeSort.Sort(array, 0, n - 1);
+                        Console.Write("\nSorted Array is: ");
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write(array[i] + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                }
+            }
+        }
+    }
+}
